0996874a5b0c3152712012f11453720b9c0433f3
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-04-25 13:48:11 -0400

    Resolving doubler and passing the test

--- problems.js
+++ problems.js
@@ -21,10 +21,11 @@ function doubler(nums) {
     /* Takes an array of numbers and returns a new array where every element of
     the original array is multiplied by 2. */
     // Your code here
-    let newArray = [];
 
-    for (let i=0; i <= nums; i++) {
-        newArray.push(num2 * 2);
+    var newArray = [];
+
+    for (let i=0; i < nums.length; i++) {
+        newArray.push(nums[i] * 2);
     }
 
     return newArray;
