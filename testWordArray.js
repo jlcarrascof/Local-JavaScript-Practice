0cb542d9dcb7ba4b73f6f57a65e5abe0782e6bd0
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-04-25 19:38:03 -0400

    Create testWordArray.js file

---
+++ testWordArray.js
@@ -0,0 +1,6 @@
+function wordWithinArray(word, arr) {
+    /* Takes in both a word and an array of words as arguments and returns a
+    boolean that returns true if that string is located inside of the array, or
+    false if it does not. Use `Array.indexOf`. */
+    // Your code here
+};
