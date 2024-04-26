c1dd02a03d56064e300472e4c5ef7c5fbbfbe460
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-04-26 11:54:16 -0400

    Create testFizzBuzz.js file

---
+++ testFizzBuzz.js
@@ -0,0 +1,5 @@
+function fizzBuzz(max) {
+    /* Takes a number, max and returns an array that contains every number from
+    0 to max (not inclusive) that is divisible by either 3 or 5, **but not both**. */
+    // Your code here
+};
