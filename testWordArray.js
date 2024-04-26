5ea66628c61e719ba486358e14cd020b53e78ac5
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-04-25 20:47:32 -0400

    Finish and run testWordArray.js file

--- testWordArray.js
+++ testWordArray.js
@@ -3,4 +3,17 @@ function wordWithinArray(word, arr) {
     boolean that returns true if that string is located inside of the array, or
     false if it does not. Use `Array.indexOf`. */
     // Your code here
+
+    var foundWord = arr.indexOf(word);
+
+    return foundWord;
+
 };
+
+result = wordWithinArray('Stanton', ['Cortes', 'Soto', 'Verdugo', 'Stanton', 'Judge']);
+
+if (result !== -1) {
+    console.log("I found the word in the array");
+} else {
+    console.log("I couldn't find the word in the array");
+}
