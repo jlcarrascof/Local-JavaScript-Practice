f15c3685bf990419f254acd22bef230cf77fa83b
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-04-26 12:40:39 -0400

    Finish and run testFizzBuzz.js file

--- problems.js
+++ problems.js
@@ -72,6 +72,17 @@ function fizzBuzz(max) {
     /* Takes a number, max and returns an array that contains every number from
     0 to max (not inclusive) that is divisible by either 3 or 5, **but not both**. */
     // Your code here
+
+    const finalArray = [];
+    for (let i=1; i < max; i++) {
+        if (i % 3 === 0) {
+            finalArray.push(i);
+        } else if (i % 5 === 0) {
+            finalArray.push(i);
+        }
+    }
+
+    return finalArray;
 };
 
 function hello(name) {
--- testFizzBuzz.js
+++ testFizzBuzz.js
@@ -2,4 +2,22 @@ function fizzBuzz(max) {
     /* Takes a number, max and returns an array that contains every number from
     0 to max (not inclusive) that is divisible by either 3 or 5, **but not both**. */
     // Your code here
+
+    const finalArray = [];
+    for (let i=1; i <= max; i++) {
+        if (i % 3 === 0) {
+            finalArray.push(i);
+            if (i % 5 === 0) {
+                finalArray.pop(i);
+            }
+        } else if (i % 5 === 0) {
+            finalArray.push(i);
+        }
+    }
+
+    return finalArray;
+
 };
+
+result = fizzBuzz(30);
+console.log(result);
